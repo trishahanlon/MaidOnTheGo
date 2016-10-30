d345ebf54f57fb8c53b2bdcffead57cc224aa834
trishamoyer <trishamoyer10@gmail.com>
2016-10-29 23:26:58 -0500

    breaking things. yay

--- app/src/main/java/com/uis/thanl2/maidonthego/MainActivity.java
+++ app/src/main/java/com/uis/thanl2/maidonthego/MainActivity.java
@@ -4,11 +4,24 @@ import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton cottonButton, denimButton, linenButton, nylonButton, polyesterButton, satinButton, silkButton, woolButton;
+    TextView nameOfStain;
+    ListView stainListView;
+    private static final String[] stains = {"BBQ sauce", "Baseball dirt", "Blood", "Chewing gum", "Chocolate", "Coffee", "Cranberry Juice", "Grass Stains", "Grease", "Ink", "Ketchup", "Kool Aid",  "Lip stick",  "Mustard",  "Soy Sauce",  "Strawberry Jam",  "Sweat Tea",  "Tomato Sauce",  "Urine",  "Wine"};
+    Map <String,String> map;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +37,28 @@ public class MainActivity extends AppCompatActivity {
         satinButton = (ImageButton) this.findViewById(R.id.satinButton);
         silkButton = (ImageButton) this.findViewById(R.id.silkButton);
         woolButton = (ImageButton) this.findViewById(R.id.woolButton);
+
+
+        nameOfStain = (TextView) this.findViewById(R.id.nameOfStain);
+        stainListView = (ListView) this.findViewById(R.id.stainListView);
+
+        //hashmap
+        map =  new HashMap<String,String>();
+
+        final ArrayList <String> stainList = new ArrayList<>();
+        for (int i = 0; i < stains.length; i++){
+            stainList.add(stains[i]);
+        }
+
+        final ArrayAdapter <String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, stainList);
+        stainListView.setAdapter(adapter);
+
+        stainListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                System.out.println("hi there");
+            }
+        });
     }
 
     public void selectCotton(View view) {
@@ -57,7 +92,4 @@ public class MainActivity extends AppCompatActivity {
     public void selectWool(View view) {
         System.out.println("hi from wool");
     }
-
-
-
 }

0070923dc500895077a7ab62fcc34c761fc9a295
Trisha Moyer <tehanlon@us.ibm.com>
2016-10-29 21:33:14 -0500

    initial commit

---
+++ app/src/main/java/com/uis/thanl2/maidonthego/MainActivity.java
@@ -0,0 +1,13 @@
+package com.uis.thanl2.maidonthego;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
